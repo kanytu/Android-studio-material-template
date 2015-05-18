dec1c0dafbf9e35f4665070637ee51211bcbf6ad
Trung Vo <mtrung@gmail.com>
2015-05-18 00:18:33 -0500

    new way to install template

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 # Created by Trung Vo
 # Simple install script
-Android_Studio_installation_dir="/Applications/Android Studio.app/Contents"
-dest=${Android_Studio_installation_dir}/plugins/android/lib/templates/activities/
-cp -R MaterialNavigationDrawerActivity "$dest"/MaterialNavigationDrawerActivity
-ls "$dest"/MaterialNavigationDrawerActivity
+
+# old way
+#Android_Studio_installation_dir="/Applications/Android Studio.app/Contents"
+#dest_dir=${Android_Studio_installation_dir}/plugins/android/lib/templates/activities/
+
+template_name=MaterialNavigationDrawerActivity
+
+echo Android SDK=$ANDROID_HOME
+if [ -z "$ANDROID_HOME" ]; then
+    echo "ANDROID_HOME is empty"
+    exit
+fi
+dest_dir=$ANDROID_HOME/extras/templates/activities
+
+cp -R $template_name "$dest_dir"/$template_name
+ls "$dest_dir/$template_name"
